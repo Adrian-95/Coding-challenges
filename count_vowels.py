3565fe6c19af58d3694d899fb23673a58d7f164d
Adrian-95 <52085415+Adrian-95@users.noreply.github.com>
2020-09-21 23:04:04 +0200

    Add files via upload

---
+++ count_vowels.py
@@ -0,0 +1,10 @@
+# Return the number (count) of vowels in the given string.
+# The input string will only consist of lower case letters and/or spaces.
+
+def get_count(input_str):
+    num_vowels = 0
+    # your code here
+    for char in input_str:
+        if char in "aeiouAEIOU":
+            num_vowels = num_vowels+1
+    return num_vowels
